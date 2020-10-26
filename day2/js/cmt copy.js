ddf057d944668d3d1221f5f87f186a3ce980173b
童永泽 <604232797@qq.com>
2020-10-26 21:13:30 +0800

    新闻列表

--- day2/js/cmt copy.js
+++ day2/js/cmt copy.js
@@ -2,19 +2,20 @@ function getCommentList() {
   $.ajax({
     type: 'GET',
     url: 'http://www.liulongbin.top:3006/api/cmtlist',
-    // data:{}  无
-    success: function (res) {
-      // console.log(res);
+    success(res) {
       if (res.status !== 200) return alert('获取评论列表失败')
       // console.log('获取数据成功');
       var rows = []
-      $.each(res.data, function (i, item) {
-        var str = '<li class="list-group-item"><span class="badge" style="background-color: #F0AD4E;">评论时间：' + item.time + '</span><span class="badge" style="background-color: #5BC0DE;">评论人：' + item.username + '</span>' + item.content + '</li>'
-        rows.push(str)
+      res.data.forEach(item => {
+        // 拼接html字符串
+        rows.push(`<li class="list-group-item">
+        <span class="badge" style="background-color: #F0AD4E;">评论时间：${item.time}</span>
+        <span class="badge" style="background-color: #5BC0DE;">评论人：${item.username}</span>
+        ${item.content}
+      </li>`)
       })
-      $('#cmt-list').empty().append(rows.join(''))
+      $('#cmt-list').empty().append(rows)
     }
-
   })
 }
 getCommentList()
@@ -24,13 +25,18 @@ $(function () {
     e.preventDefault()
     var data = $(this).serialize()
     // console.log(data);
-    $.post('http://www.liulongbin.top:3006/api/addcmt', data, function (res) {
-      if (res.status !== 201) {
-        return alert('发表评论失败!')
-      }
-      getCommentList()  //刷新表单列表
-      $('#formAddCmt')[0].reset()  // reset  表单form 里面的重置表单   把jq 转换为原生dom对象
-    })
 
+    // 请求
+    $.ajax({
+      type: 'POST',
+      url: 'http://www.liulongbin.top:3006/api/addcmt',
+      data,
+      success(res) {
+        if (res.status !== 201) return alert('发表评论失败')
+        getCommentList()
+        $('#formAddCmt').get(0).reset()
+      }
+    })
+    return false
   })
 })
---
+++ day2/案例-新闻列表/js/news copy.js
@@ -0,0 +1,37 @@
+$(function () {
+  // 获取新闻列表的函数
+  function getNewList() {
+    template.defaults.imports.formatDate = function (data) {
+      let date = new Date(data)
+      let year = date.getFullYear()
+
+      let month = date.getMonth() + 1
+      month = ('' + month).padStart(2, 0)
+
+      let day = date.getDate()
+      day = day.toString().padStart(2, 0)
+
+      let hours = date.getHours()
+      hours = hours.toString().padStart(2, 0)
+
+      let minutes = date.getMinutes()
+      minutes = minutes.toString().padStart(2, 0)
+
+      let seconds = date.getSeconds()
+      seconds = seconds.toString().padStart(2, 0)
+      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+    }
+
+    $.get('http://www.liulongbin.top:3006/api/news', function (res) {
+      if (res.status !== 200) {
+        return alert('获取新闻列表失败')
+      }
+      res.data.forEach(item => item.tags = item.tags.split(','))
+
+      $('#news-list').html(template('news-tmp', res))
+    })
+
+  }
+
+  getNewList()
+})
